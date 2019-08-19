89d8cd9fb8027dda57d65fa5173bb571e3d3cec7
hutl <1534950128@qq.com>
2019-08-19 16:25:48 +0800

    产生冲突

--- src/zion/test1.java
+++ src/zion/test1.java
@@ -4,8 +4,7 @@ public class test1 {
 	public static void main(String[] args) {
 		System.out.println("现在是第一步，进行测试");
 		System.out.println("添加一条数据");
-
-		System.out.println("开始产生冲突");
+		System.out.println("现在两者不一样了，产生冲突");
 
 }
 }
