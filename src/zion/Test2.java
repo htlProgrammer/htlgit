fc919dceec6d4017c2a8fe280d0a7041cc00d4cf
hutl <1534950128@qq.com>
2019-08-19 16:02:25 +0800

    添加一个文件和一条数据

---
+++ src/zion/Test2.java
@@ -0,0 +1,7 @@
+package zion;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("这里啥都不写");
+	}
+}
--- src/zion/test1.java
+++ src/zion/test1.java
@@ -3,5 +3,6 @@ package zion;
 public class test1 {
 	public static void main(String[] args) {
 		System.out.println("现在是第一步，进行测试");
+		System.out.println("添加一条数据");
 	}
 }
