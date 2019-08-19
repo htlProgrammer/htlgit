4bc3590ae19ec462351472b7b588561d0fa856f7
htlProgrammer <51787955+htlProgrammer@users.noreply.github.com>
2019-08-19 16:38:13 +0800

    Update test1.java

--- src/zion/test1.java
+++ src/zion/test1.java
@@ -2,10 +2,8 @@ package zion;
 
 public class test1 {
 	public static void main(String[] args) {
-		System.out.println("现在是第一步，进行测试");
-		System.out.println("添加一条数据");
-		System.out.println("现在两者不一样了，产生冲突");
 		System.out.println("人生得意须尽欢");
+		System.out.println("一行bug跑一天");
 
 
 }
