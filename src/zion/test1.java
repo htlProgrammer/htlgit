a2b6cae6ffbb532dd67a3d982b9dd1286add8576
hutl <1534950128@qq.com>
2019-08-19 16:21:48 +0800

    冲突

--- src/zion/test1.java
+++ src/zion/test1.java
@@ -4,7 +4,6 @@ public class test1 {
 	public static void main(String[] args) {
 		System.out.println("现在是第一步，进行测试");
 		System.out.println("添加一条数据");
-		System.out.println("开始产生chongtu");
-		System.out.println("开始产生chongtu");
+		System.out.println("开始产生冲突");
 	}
 }
