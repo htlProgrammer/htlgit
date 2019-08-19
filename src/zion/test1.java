a8f8a095d3e1c0cf1895e3f6970f0e50f16fc671
hutl <1534950128@qq.com>
2019-08-19 16:30:48 +0800

    测试bug

--- src/zion/test1.java
+++ src/zion/test1.java
@@ -5,10 +5,8 @@ public class test1 {
 		System.out.println("现在是第一步，进行测试");
 		System.out.println("添加一条数据");
 
-		System.out.println("现在两者不一样了，产生冲突");
 		
-		System.out.println("开始产生冲突");
-		System.out.println("假装修改成功");
+		System.out.println("人生得意须尽欢");
 
 }
 }
